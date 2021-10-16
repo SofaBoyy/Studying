4c409a235e2c432ab6d87a16880536fe6a384d55
Your Name <nickk.lima17@gmail.com>
2021-10-16 15:29:54 -0300

    3q ativ 5 done

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 import { connect, disconnect } from 'mongoose';
 import * as AutorRepositorio from './persistencia/autorRepositorio';
 
-const uri = 'mongodb+srv://dbUser:kil,ji8oku@cluster0.wszic.mongodb.net/meubd?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://nick:nick@atividade5db.54mse.mongodb.net/Atividade5DB?retryWrites=true&w=majority';
 
 async function main() {
     try {
         const cliente = await connect(uri);
         console.log('Conectado ao MongoDb Atlas');
 
+        /*
         console.log('Adicionando autores...');
-        let a1 = await AutorRepositorio.criar({primeiro_nome: 'John', ultimo_nome: 'Doe'});
+        let a1 = await AutorRepositorio.criar({primeiro_nome: 'John', ultimo_nome: 'Matutu'});
         console.log(`Autor inserido: ${a1}`);
         let a2 = await AutorRepositorio.criar({primeiro_nome: 'Mary', ultimo_nome: 'Doe'});
         console.log(`Autor inserido: ${a2}`);
+        */
 
-        console.log('Buscando autores...');
-        let autores = await AutorRepositorio.buscar();
-        autores.forEach(autor => console.log(autor));
-
+        console.log('Alterando altores...');
+        let autores = await AutorRepositorio.alterarRegistro("616b0ed607c4eed792b7e580", "Nicolau", "Cara de Pau");
+        console.log(autores);
     } catch (error) {
         console.log(`Erro: ${error}`);
     } finally {
--- src/persistencia/autorRepositorio.ts
+++ src/persistencia/autorRepositorio.ts
@@ -9,3 +9,29 @@ export async function buscar(): Promise<Autor[]> {
     let consulta = AutorModel.find();
     return consulta.exec(); //retorna uma Promise
 }
+
+export async function buscarPorUltimoNome(lastName: string): Promise<Autor[]> {
+    let consulta = await AutorModel.find({ultimo_nome: lastName}).exec();
+    return consulta;
+}
+
+export async function buscarPorPrimeiroNome(firstName:string): Promise<Autor[]> {
+    let consulta = await AutorModel.find({primeiro_nome: firstName}).exec();
+    return consulta;
+}
+
+export async function alterarRegistro(_id: string, firstName?: string, lastName?: string) {
+    if(firstName != undefined && lastName != undefined) {
+        let consulta = await AutorModel.findById(_id).updateOne({primeiro_nome: firstName, ultimo_nome: lastName}).exec();
+        console.log("Primeiro e último nome alterado com sucesso!");
+    }else if(firstName != undefined) {
+        let consulta = await AutorModel.findById(_id).updateOne({primeiro_nome: firstName}).exec();
+        console.log("Primeiro nome alterado com sucesso!")
+    }else if(lastName != undefined) {
+        let consulta = await AutorModel.findById(_id).updateOne({ultimo_nome: lastName}).exec();
+        console.log("Ultimo nome alterado com sucesso!")
+    }
+
+    return await AutorModel.findById(_id).exec();
+}
+
